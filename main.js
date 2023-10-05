d803de3359f0357d01e23e41e5ef42fc68d397e0
Viviane <106716101+profvivi@users.noreply.github.com>
2023-10-05 19:40:13 -0300

    s

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 
 
-function tocaSomAplausos(){
-    document.querySelector(‘#som_tecla_aplausos’).play();
+function tocaSomRisada(){
+    document.querySelector(‘#som_tecla_risada’).play();
     }
+
+    document.querySelector(‘.tecla_pom’).onclick = tocaSomRisada;

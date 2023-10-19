09429fe140949bed3134ec1b272e01b7a8867f58
Viviane <106716101+profvivi@users.noreply.github.com>
2023-10-19 19:47:29 -0300

    s

--- main.js
+++ main.js
@@ -2,8 +2,29 @@
 
 
 
-function tocaSomRisada(){
-    document.querySelector('#som_tecla_risada').play();
+function tocaSomAplausos(){
+    document.querySelector('#som_tecla_aplausos').play();
     }
 
-    document.querySelector('.tecla_risada').onclick = tocaSomRisada;
+    function tocaSomVaia(){
+        document.querySelector('#som_tecla_vaia').play();
+        }
+        function tocaSomRisada(){
+            document.querySelector('#som_tecla_risada').play();
+            }
+
+    //document.querySelector('.tecla_risada').onclick = tocaSomRisada;
+
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+    listaDeTeclas[0].onclick = tocaSomAplausos;
+    listaDeTeclas[1].onclick = tocaSomVaia;
+    listaDeTeclas[2].onclick = tocaSomRisada;
+
+
+
+
+/* function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+      
+const listaDeTeclas = document.querySelectorAll('.tecla');*/

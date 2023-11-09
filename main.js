90486d5b2ef9a872ac9770be750e4e4bb09aa45a
Viviane <106716101+profvivi@users.noreply.github.com>
2023-11-09 19:15:40 -0300

    s

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+/*function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+   
+}*/
+
+
+/*
 function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
   }
@@ -23,4 +42,19 @@ function tocaSom(idElementoAudio) {
     contador = contador + 1; //então conta
   
     console.log(contador);//imprimir o contador várias vezes
-  };
+  };*/
+
+  function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+}

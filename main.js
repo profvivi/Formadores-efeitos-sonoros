6a7bedc5e2d2ca0432aeda4c412364cb9609ec56
Viviane <106716101+profvivi@users.noreply.github.com>
2023-11-14 20:31:06 -0300

    s

--- main.js
+++ main.js
@@ -60,4 +60,7 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
     tecla.onkeydown = function(){
       tecla.classList.add('ativa')
    }
+   tecla.onkeyup = function(){
+    tecla.classList.remove('ativa')
+ }
 }

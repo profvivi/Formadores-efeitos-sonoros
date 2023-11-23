10d372bd92d26ea33dc299a7cee4130e70a9d213
Viviane <106716101+profvivi@users.noreply.github.com>
2023-11-23 19:38:07 -0300

    s

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
     }
     tecla.onkeydown = function (evento) {
 
-      if (evento.code === 'Space' || evento.code === 'Enter') {
+      if (evento.code === 'Space') {
           tecla.classList.add('ativa');
       }
 

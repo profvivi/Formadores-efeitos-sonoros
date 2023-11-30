522dfa5c1014b49872a8b00ea52b4d607cc3af02
Viviane <106716101+profvivi@users.noreply.github.com>
2023-11-30 19:02:08 -0300

    s

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+    const idAudio = `#som_${instrumento}`; 
 
     tecla.onclick = function () {
         tocaSom(idAudio);

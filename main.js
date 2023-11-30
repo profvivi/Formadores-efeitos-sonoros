14457943d91c8904bcc21463ef0faf33f034de71
Viviane <106716101+profvivi@users.noreply.github.com>
2023-11-30 19:01:04 -0300

    s

--- main.js
+++ main.js
@@ -44,31 +44,42 @@ function tocaSom(idElementoAudio) {
     console.log(contador);//imprimir o contador várias vezes
   };*/
 
-  function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+  function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
-const listaDeTeclas = document.querySelectorAll(".tecla");
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-for(let contador = 0; contador < listaDeTeclas.length; contador++){
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
     const tecla = listaDeTeclas[contador];
-    const efeito = tecla.classList[1];
-    const idAudio = `#som_${efeito}`;
-    tecla.onclick = function (){
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
+
     tecla.onkeydown = function (evento) {
 
-      if (evento.code === 'Space'|| evento.code === 'Enter') {
-          tecla.classList.add('ativa');
-      }
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
 
-  }
+    }
 
-  tecla.onkeyup = function () {
-      tecla.classList.remove('ativa');
-  }
-
-}
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
